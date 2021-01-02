6f43673dd0c6e365a40a1f85359c17d55ed0109a
Whitney Harden <wtharden320@gmail.com>
2021-01-02 12:44:51 -0500

    typing Auth and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import { Component } from 'react';
 import './App.css';
+import Auth from './Components/Auth/Auth'
 
-const App: React.FunctionComponent = () => {
+//using alias over interfaces //https://medium.com/@koss_lebedev/type-aliases-vs-interfaces-in-typescript-based-react-apps-e77c9a1d5fd0
+type AppState = {
+  token: string
+}
+
+class App extends Component<{}, AppState> {
+  constructor(props: any){
+    super(props);
+    this.state = {
+      token: ''
+    }
+  }
+  render(){
   return (
     <div className="App">
       <header className="App-header">
         The Bee Lounge -dev
+        <hr />
+        {/* <Auth token={this.state.token}/> */}
       </header>
     </div>
-  );
+  )
+
+  }
 }
 
 export default App;
---
+++ src/Components/Auth/Auth.tsx
@@ -0,0 +1,36 @@
+import { Component } from 'react';
+
+type AuthProps = {
+  props: any;
+};
+
+type AuthState = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  userRole: string;
+};
+
+class Auth extends Component<AuthProps, AuthState> {
+  constructor(props: AuthProps) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      userRole: '',
+    };
+  }
+  render(){
+    return(
+
+    <div>
+      Auth
+    </div>
+    )
+  }
+}
+
+export default Auth;
