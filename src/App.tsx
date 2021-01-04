2dcaff8711759d541e5285e5f7e985a307a79dff
Whitney Harden <wtharden320@gmail.com>
2021-01-04 09:59:15 -0500

    fixed type issue passing down token

--- src/App.tsx
+++ src/App.tsx
@@ -10,24 +10,27 @@ import Auth from './Components/Auth/Auth'
 //   token: string;
 // }
 
-// type AppState = {
-//   token: string
-// }
+type AppState = {
+  token: string
+}
 
-class App extends Component<{}, {}> {
+class App extends Component<{}, AppState> {
   constructor(props: any){
     super(props);
     this.state = {
-      token: 'ball'
+      token: 'test token'
     }
   }
   render(){
+    const tokenProps ={token: this.state.token}
+    
   return (
     <div className="App">
       <header className="App-header">
         The Bee Lounge
+        <p>{this.state.token}</p>
         <hr />
-        <Auth />
+        <Auth {...tokenProps}/>
         <hr />
         <Home />
       </header>
